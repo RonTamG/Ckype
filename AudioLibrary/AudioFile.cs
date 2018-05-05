38919e0edfa86b71c83da8229ab61ba5daa2c969
RonTamG <rontam987@gmail.com>
2018-05-06 01:54:19 +0300

    AudioLibrary and Caliburn.micro foundation added

---
+++ AudioLibrary/AudioFile.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using NAudio.Wave;
+
+
+namespace AudioLibrary
+{
+    public static class AudioFile
+    {
+        #region Private Variables
+
+        private static AudioFileReader audioReader = null;
+        private static DirectSoundOut soundOut = null;
+        private static WaveInEvent soundIn;
+        private static WaveFileWriter waveWriter = null;
+
+        #endregion
+
+        #region Playing
+
+        /// <summary>
+        /// Starts playing the given file
+        /// 
+        /// If you input a filename then the file will open and start playing
+        /// Otherwise it plays the currently open file
+        /// </summary>
+        /// <param name="filename">The path of the file to play, DEFAULT = NULL</param>
+        public static void PlayFile(string filename = null)
+        {
+            if (filename != null)
+            {
+                soundOut = new DirectSoundOut();
+                soundOut.PlaybackStopped += PlaybackStopped;
+                audioReader = new AudioFileReader(filename);
+                soundOut.Init(audioReader);
+            }
+            soundOut.Play();
+        }
+
+        /// <summary>
+        /// Pauses the currently open audio file, if it exists
+        /// </summary>
+        public static void PauseFile()
+        {
+            soundOut?.Pause();
+        }
+
+        /// <summary>
+        /// Disposes of all current active file resources
+        /// </summary>
+        public static void CloseFile()
+        {
+            soundOut?.Pause();
+            soundOut.PlaybackStopped -= PlaybackStopped;
+            soundOut?.Dispose();
+            audioReader?.Dispose();
+        }
+
+        private static void PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            // clear output device
+            soundOut.Dispose();
+            soundOut = null;
+        }
+        #endregion
+
+        #region Recording
+
+        /// <summary>
+        /// Record audio to file
+        /// </summary>
+        /// <param name="outputFilePath">Path to recorded file</param>
+        public static void Record(string outputFilePath)
+        {
+            soundIn = new WaveInEvent();
+            waveWriter = new WaveFileWriter(outputFilePath, soundIn.WaveFormat);
+
+            soundIn.StartRecording();
+            soundIn.DataAvailable += DataAvailable;
+        }
+
+        /// <summary>
+        /// Stop recording if the sound in variable exists
+        /// </summary>
+        public static void StopRecording()
+        {
+            soundIn?.StopRecording();
+            soundIn.DataAvailable -= DataAvailable;
+            waveWriter?.Dispose();
+            waveWriter = null;
+            soundIn.Dispose();
+        }
+
+        private static void DataAvailable(object sender, WaveInEventArgs e)
+        {
+            waveWriter.Write(e.Buffer, 0, e.BytesRecorded);
+        }
+        #endregion
+    }
+}
---
+++ AudioLibrary/NetworkAudio.cs
@@ -0,0 +1,135 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace AudioLibrary
+{
+    public class NetworkAudio
+    {
+        #region Variables
+
+        #region Socket
+        private UdpClient udpListener;
+        private UdpClient udpSender;
+        private IPEndPoint connectionEndPoint;
+        #endregion
+        #region Sound
+        private BufferedWaveProvider waveProvider;
+        private DirectSoundOut soundOut = null;
+        private WaveInEvent SoundIn = null;
+        #endregion
+        #region User
+        private bool connected = false;
+        private bool muted = false;
+        #endregion
+
+        #endregion
+
+        #region Constructor
+
+        public NetworkAudio(IPEndPoint ConnectToEndPoint)
+        {
+            connectionEndPoint = ConnectToEndPoint;
+        }
+
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Toggle if audio from this computer is sent
+        /// </summary>
+        public void ToggleSelfMute()
+        {
+            muted = !muted;
+        }
+
+        /// <summary>
+        /// Disconnect from other client and dispose of all variables
+        /// </summary>
+        public void Disconnect()
+        {
+            if (connected)
+            {
+                soundOut.Dispose();
+                soundOut = null;
+                SoundIn.DataAvailable -= OnAudioCaptured;
+
+                SoundIn.StopRecording();
+                SoundIn.Dispose();
+                SoundIn = null;
+
+                udpListener?.Close();
+                udpListener = null;
+                udpSender?.Close();
+                udpSender = null;
+            }
+            connected = false;
+        }
+
+        /// <summary>
+        /// Connects to other client and starts transmitting audio
+        /// </summary>
+        public void Start()
+        {
+            udpSender = new UdpClient();
+            SoundIn = new WaveInEvent();
+            SoundIn.DataAvailable += OnAudioCaptured;
+            SoundIn.StartRecording();
+            udpListener = new UdpClient();
+            udpListener.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true); // test
+            //
+            var EndPointListen = new IPEndPoint(IPAddress.Any, connectionEndPoint.Port);
+            udpListener.Client.Bind(EndPointListen);
+
+            udpSender.Connect(connectionEndPoint);
+
+
+            soundOut = new DirectSoundOut();
+            waveProvider = new BufferedWaveProvider(SoundIn.WaveFormat);
+            soundOut.Init(waveProvider);
+            soundOut.Play();
+
+            ThreadPool.QueueUserWorkItem(ListenerThread, EndPointListen);
+
+            connected = true;
+        } 
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Function to run the listener on a different thread
+        /// </summary>
+        /// <param name="endPoint">The endpoint the listener is connected to [TYPE:IPEndPoint]</param>
+        private void ListenerThread(object endPoint)
+        {
+            var endPointTemp = (IPEndPoint)endPoint;
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = udpListener.Receive(ref endPointTemp);
+                    waveProvider?.AddSamples(buffer, 0, buffer.Length);
+                }
+            }
+            catch (SocketException)
+            {
+                // usually not a problem - just means we have disconnected
+
+            }
+        }
+
+        private void OnAudioCaptured(object sender, WaveInEventArgs e)
+        {
+            if (!muted)
+                udpSender.Send(e.Buffer, e.BytesRecorded);
+        } 
+
+        #endregion
+    }
+}
